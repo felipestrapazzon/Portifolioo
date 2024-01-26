9a0dc5ef65efca6d97415a5843fa86e36fc5351f
felipestrapazzon <102249827+felipestrapazzon@users.noreply.github.com>
2024-01-26 20:40:38 -0300

    Add files via upload

---
+++ js/error.js
@@ -0,0 +1,53 @@
+/*document.getElementById("playButton").addEventListener("click", function() {
+    var audio = document.getElementById("audioPlayer");
+    audio.play();
+  });
+
+
+
+  document.getElementById("audioLink").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    
+    var audio = document.getElementById("audioPlayer2");
+    audio.play();
+  });
+
+
+  document.getElementById("audioLink2").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    
+    var audio = document.getElementById("audioPlayer3");
+    audio.play();
+  });*/
+  
+
+
+  
+
+ // PRIVACY
+ document.getElementById("audioLink3").addEventListener("click", function(event) {
+  event.preventDefault(); // Impede o comportamento padrão do link
+  var audio = document.getElementById("audioPlayer4");
+  audio.play();
+});
+
+  // DETALHES
+  document.getElementById("audioLink2").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    var audio = document.getElementById("audioPlayer3");
+    audio.play();
+  });
+
+  // BUTTON
+  document.getElementById("playButton").addEventListener("click", function() {
+    var audio = document.getElementById("audioPlayer");
+    audio.play();
+  });
+
+  
+
+  
+
+ 
+
+
---
+++ js/main.js
@@ -0,0 +1,48 @@
+/*CÓDIGO SOBRE ANIMAÇÃO*/
+
+const letterContainers = document.querySelectorAll('.letter-container');
+
+setInterval(() => {
+  letterContainers.forEach(container => {
+    container.classList.add('spin');
+    setTimeout(() => {
+      container.classList.remove('spin');
+    }, 5000)
+  });
+}, 10000);
+
+//CODIGO MUSICA DE FUNDO
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  var audio = document.getElementById('background-music');
+  audio.volume = 0.04;
+  audio.play();
+});
+
+
+
+//CODIGO TROCAR IMAGEM E MUTAR MUSICA
+
+let imgAtual = "img/SOUND/ONSOUND.jpg"
+let imgDps = "img/SOUND/MUTED.jpg"
+let musicaMuda = true;
+
+function trocarImagem() {
+
+  document.getElementById("penis").src = imgAtual;
+  let aux = imgAtual
+  imgAtual = imgDps
+  imgDps = aux
+
+  var audio = document.getElementById('background-music');
+  if (musicaMuda) {
+    audio.volume = 0;
+  } else {
+    audio.volume = 0.04;
+  }
+
+  musicaMuda = !musicaMuda
+
+};
+
+
