17f718998d24bd87ebfecd150fd9fe610ef30fe3
felipestrapazzon <102249827+felipestrapazzon@users.noreply.github.com>
2024-02-17 14:35:10 -0300

    main

--- js/main.js
+++ js/main.js
@@ -13,36 +13,58 @@ setInterval(() => {
 
 //CODIGO MUSICA DE FUNDO
 
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   var audio = document.getElementById('background-music');
+//   audio.volume = 0.04;
+//   audio.play();
+// });
+
+
+
+// //CODIGO TROCAR IMAGEM E MUTAR MUSICA
+
+// let imgAtual = "img/SOUND/ONSOUND.jpg"
+// let imgDps = "img/SOUND/MUTED.jpg"
+// let musicaMuda = true;
+
+// function trocarImagem() {
+
+//   document.getElementById("penis").src = imgAtual;
+//   let aux = imgAtual
+//   imgAtual = imgDps
+//   imgDps = aux
+
+//   var audio = document.getElementById('background-music');
+//   if (musicaMuda) {
+//     audio.volume = 0;
+//   } else {
+//     audio.volume = 0.04;
+//   }
+
+//   musicaMuda = !musicaMuda
+
+// };
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
   var audio = document.getElementById('background-music');
   audio.volume = 0.04;
   audio.play();
 });
 
-
-
-//CODIGO TROCAR IMAGEM E MUTAR MUSICA
-
-let imgAtual = "img/SOUND/ONSOUND.jpg"
-let imgDps = "img/SOUND/MUTED.jpg"
-let musicaMuda = true;
+let imgMuted = "img/SOUND/MUTED.jpg";
+let imgOnSound = "img/SOUND/ONSOUND.jpg";
+let isMuted = false;
 
 function trocarImagem() {
-
-  document.getElementById("penis").src = imgAtual;
-  let aux = imgAtual
-  imgAtual = imgDps
-  imgDps = aux
-
   var audio = document.getElementById('background-music');
-  if (musicaMuda) {
-    audio.volume = 0;
+  isMuted = !isMuted;
+  
+  if (isMuted) {
+      audio.volume = 0;
+      document.getElementById("penis").src = imgMuted;
   } else {
-    audio.volume = 0.04;
+      audio.volume = 0.04;
+      document.getElementById("penis").src = imgOnSound;
   }
-
-  musicaMuda = !musicaMuda
-
 };
-
-
